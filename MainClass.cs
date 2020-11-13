d28b806eb1b41cf88d18bbe5489c397943cb52ec
Colin Thomas <cthomas0687@gmail.com>
2020-11-13 00:40:52 -0500

    First commit

---
+++ MainClass.cs
@@ -0,0 +1,57 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace GmailAPIProject
+{
+    class MainClass
+    {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+
+
+
+
+            Program.ReadEmails("mailbotmailbotmailbot@gmail.com", credential);
+            Console.WriteLine("Done!");
+        }
+    }
+}
+
+
+/*Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            AE.Net.Mail.MailMessage msg = Program.CreateMessage(
+                "Dear Colin",
+                "How are you",
+                "mailbotmailbotmailbot@gmail.com",
+                "cthomas0687@gmail.com");*/
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using Microsoft.AspNetCore.WebUtilities;
+
+
+namespace GmailAPIProject
+{
+    static class Program
+    {
+        static string ApplicationName = "Gmail API .NET Quickstart";
+
+        public static void SendIt(UserCredential credential, AE.Net.Mail.MailMessage msg)
+        {
+            var gmail = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            var msgStr = new StringWriter();
+            msg.Save(msgStr);
+
+            var result = gmail.Users.Messages.Send(new Message
+            {
+                Raw = Base64UrlEncode(msgStr.ToString())
+            }, "me").Execute();
+            Console.WriteLine("Message ID {0} sent.", result.Id);
+        }
+
+        public static void ReadEmails(String sender, UserCredential credential)
+        {
+            var gmail = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            var request = gmail.Users.Messages.List("me");
+            request.IncludeSpamTrash = false;
+            request.LabelIds = "INBOX";
+
+            var responseList = request.Execute();
+            var ids = new List<String>();
+
+            foreach(Message m in responseList.Messages)
+            {
+                ids.Add(m.Id);
+            }
+
+            for (int i = 0; i < ids.Count-1; i++)
+            {
+                var msgRequest = gmail.Users.Messages.Get("me", ids[i]);
+                //msgRequest.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Raw;
+                
+                Message? msgResponse = msgRequest.Execute();
+                String? body = msgResponse.Payload.Body.Data;
+
+
+                if (body != null)
+                {
+                    Console.WriteLine(System.Text.Encoding.UTF8.GetString(
+                    WebEncoders.Base64UrlDecode(body)));
+                }
+            }
+
+
+        }
+
+        public static AE.Net.Mail.MailMessage CreateMessage(String subject, String body, String fromAddress, String tooAddress)
+        {
+            var msg = new AE.Net.Mail.MailMessage
+            {
+                Subject = subject,
+                Body = body,
+                From = new MailAddress(fromAddress)
+            };
+            msg.To.Add(new MailAddress("cthomas0687@gmail.com"));
+            msg.ReplyTo.Add(msg.From); // Bounces without this!!
+            var msgStr = new StringWriter();
+            msg.Save(msgStr);
+
+            return msg;
+        }
+
+        private static string Base64UrlEncode(string input)
+        {
+            var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            // Special "url-safe" base64 encode.
+            return Convert.ToBase64String(inputBytes)
+              .Replace('+', '-')
+              .Replace('/', '_')
+              .Replace("=", "");
+        }
+    }
+}
